b3b75b0dde86468075aa09ed8366dad1924ed302
jadenlee10 <jlee0032@mymail.lausd.net>
2024-09-12 19:09:30 -0700

    Create NeighborhoodRunner.java

---
+++ NeighborhoodRunner.java
@@ -0,0 +1,12 @@
+import org.code.neighborhood.*;
+
+public class NeighborhoodRunner {
+  public static void main(String[] args) {
+StevePainter jaden = new StevePainter();
+    BackgroundPainter nathan = new BackgroundPainter();
+    nathan.paintBackground("green",12);
+    jaden.drawSteve();
+
+    
+  }
+}
